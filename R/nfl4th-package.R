7c9146e67356d1f648e78905be9d47aba51c60a2
mrcaseb <roemerle@gmail.com>
2021-03-04 17:49:46 +0100

    import pipe and dplyr

--- R/nfl4th-package.R
+++ R/nfl4th-package.R
@@ -4,5 +4,7 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom magrittr %>%
+#' @import dplyr
 ## usethis namespace: end
 NULL
