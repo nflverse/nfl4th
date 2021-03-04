d0faf58e87d0a70e7933d77810bb4df97167380e
mrcaseb <roemerle@gmail.com>
2021-03-04 15:10:44 +0100

    add dependency helper

---
+++ data-raw/dependency-helper.R
@@ -0,0 +1,19 @@
+# Run this to update the DESCRIPTION
+imports <- c(
+  "magrittr",
+  # "rlang",
+  # "crayon",
+  # "cli",
+  "dplyr"
+  # "glue",
+  # "gsubfn",
+  # "purrr",
+  # "tidyr",
+  # "tibble",
+  # "furrr",
+  # "progressr",
+  # "future"
+)
+purrr::walk(imports, usethis::use_package, "Imports")
+usethis::use_tidy_description()
+rm(imports)
