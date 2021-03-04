aa7b9b854d62ade2c4478c381cb0a5cf78d54bdd
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2021-03-04 12:28:43 -0500

    start tidy eval notes

---
+++ R/silence_tidy_eval_notes.R
@@ -0,0 +1,17 @@
+posteam <-
+  qtr <-
+  half_seconds_remaining <-
+  game_seconds_remaining <-
+  end_of_half <-
+  posteam_timeouts_remaining <-
+  defteam_timeouts_remaining <-
+  away_timeouts_remaining <-
+  home_timeouts_remaining <-
+  down <-
+  ydstogo <-
+  yardline_100 <-
+  prior_posteam <-
+  score_differential <-
+  receive_2h_ko <-
+  home_team <-
+  away_team <- NULL
