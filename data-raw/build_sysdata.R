32a307b338de60beea1af21aac1abef7d22d5bda
mrcaseb <roemerle@gmail.com>
2021-03-04 18:20:48 +0100

    remove fd_model from sysdata

--- data-raw/build_sysdata.R
+++ data-raw/build_sysdata.R
@@ -1,8 +1,8 @@
 
-load("data-raw/fd_model.Rdata")
+# load("data-raw/fd_model.Rdata")
 load("data-raw/fg_model.Rdata")
 load("data-raw/two_pt_model.Rdata")
 
 punt_df <- readRDS("data-raw/punt_data.rds")
 
-usethis::use_data(fd_model, two_pt_model, fg_model, punt_df, internal = TRUE, overwrite = TRUE)
+usethis::use_data(two_pt_model, fg_model, punt_df, internal = TRUE, overwrite = TRUE)
