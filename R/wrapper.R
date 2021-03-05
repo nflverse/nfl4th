b4cef015bd6ef920fff023a87200eabcac637961
mrcaseb <roemerle@gmail.com>
2021-03-05 12:04:45 +0100

    fix example warning

--- R/wrapper.R
+++ R/wrapper.R
@@ -24,6 +24,33 @@
 #' \item{punt_wp}{Average win probability when punting.}
 #' }
 #' @export
+#' @examples
+#' \donttest{
+#' play <-
+#'   tibble::tibble(
+#'     # things to help find the right game (use "reg" or "post")
+#'     home_team = "GB",
+#'     away_team = "TB",
+#'     posteam = "GB",
+#'     type = "post",
+#'     season = 2020,
+#'
+#'     # information about the situation
+#'     qtr = 4,
+#'     quarter_seconds_remaining = 129,
+#'     ydstogo = 8,
+#'     yardline_100 = 8,
+#'     score_differential = -8,
+#'
+#'     home_opening_kickoff = 0,
+#'     posteam_timeouts_remaining = 3,
+#'     defteam_timeouts_remaining = 3
+#'   )
+#'
+#' probs <- nfl4th::add_4th_probs(play)
+#'
+#' dplyr::glimpse(probs)
+#' }
 add_4th_probs <- function(df) {
 
   original_df <- df %>% mutate(index = 1 : n())
@@ -87,6 +114,31 @@ add_4th_probs <- function(df) {
 #' \item{wp_go2}{Win probability associated with going for 2.}
 #' }
 #' @export
+#' @examples
+#' \donttest{
+#' play <-
+#'   tibble::tibble(
+#'     # things to help find the right game (use "reg" or "post")
+#'     home_team = "GB",
+#'     away_team = "TB",
+#'     posteam = "GB",
+#'     type = "post",
+#'     season = 2020,
+#'
+#'     # information about the situation
+#'     qtr = 4,
+#'     quarter_seconds_remaining = 123,
+#'     score_differential = -2,
+#'
+#'     home_opening_kickoff = 0,
+#'     posteam_timeouts_remaining = 3,
+#'     defteam_timeouts_remaining = 3
+#'   )
+#'
+#' probs <- nfl4th::add_2pt_probs(play)
+#'
+#' dplyr::glimpse(probs)
+#' }
 add_2pt_probs <- function(df) {
 
   original_df <- df %>% mutate(index = 1 : n())
