90874dd7d87f9f8f3a4337efc6f46a4264b1db5b
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2021-07-08 15:00:11 -0400

    don't need tidyverse

--- data-raw/_save_computed_numbers.R
+++ data-raw/_save_computed_numbers.R
@@ -1,14 +1,12 @@
-library(tidyverse)
+`%>%`<-magrittr::`%>%`
 
 s <- nflfastR:::most_recent_season()
-
-message(paste0(s))
 
 pbp <- nfl4th::load_4th_pbp(2014:s)
 
 condensed <- pbp %>%
-  filter(!is.na(go_boost)) %>%
-  select(
+  dplyr::filter(!is.na(go_boost)) %>%
+  dplyr::select(
     game_id, play_id, go_boost
   )
 
