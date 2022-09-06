91413d10c454d62934ca579783acb52140a0abe5
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2022-09-06 15:28:06 -0400

    keep more stuff

--- data-raw/_save_computed_numbers.R
+++ data-raw/_save_computed_numbers.R
@@ -7,7 +7,7 @@ pbp <- nfl4th::load_4th_pbp(2014:s)
 condensed <- pbp %>%
   dplyr::filter(!is.na(go_boost)) %>%
   dplyr::select(
-    game_id, play_id, go_boost, go_wp, punt_wp, fg_wp
+    game_id, play_id, go, go_boost, go_wp, punt_wp, fg_wp
   )
 
 condensed %>%
