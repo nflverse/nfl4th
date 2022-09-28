2bd987374ab4d9616889efe708fe29755e3b0609
mrcaseb <roemerle@gmail.com>
2022-09-28 16:13:45 +0200

    this will work

--- R/utils.R
+++ R/utils.R
@@ -2,14 +2,21 @@
 # replaces loading .onLoad
 init_nfl4th <- function() {
   pkg_env <- ls("package:nfl4th")
+  env <- loadNamespace("nfl4th")
 
   if (!".games_nfl4th" %in% pkg_env){
     .games_nfl4th <- get_games_file()
-    assign(".games_nfl4th", .games_nfl4th, envir = parent.env(environment()))
+    unlockBinding(".games_nfl4th", env)
+    assign(".games_nfl4th", .games_nfl4th, envir = asNamespace("nfl4th"))
+    lockBinding(".games_nfl4th", env)
   }
 
   if (!"fd_model" %in% pkg_env){
     fd_model <- load_fd_model()
-    assign("fd_model", fd_model, envir = parent.env(environment()))
+    unlockBinding("fd_model", env)
+    assign("fd_model", fd_model, envir = asNamespace("nfl4th"))
+    lockBinding("fd_model", env)
   }
 }
+
+.games_nfl4th <- fd_model <- NULL
