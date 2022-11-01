7680b7c6cfdc17d4a3bb769731701028d0739403
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2022-11-01 09:27:54 -0400

    don't do online check for removing games file

--- R/zzz.R
+++ R/zzz.R
@@ -4,14 +4,13 @@ nfl4th_fdmodel_path <- file.path(nfl4th_cache_dir, "fd_model.rds")
 nfl4th_wpmodel_path <- file.path(nfl4th_cache_dir, "wp_model.rds")
 
 .onLoad <- function(libname,pkgname){
-  is_online <- !is.null(curl::nslookup("github.com", error = FALSE))
   # create package cache directory if it doesn't exist
   if (!dir.exists(nfl4th_cache_dir)){
     dir.create(nfl4th_cache_dir, recursive = TRUE, showWarnings = FALSE)
-  } else if (file.exists(nfl4th_games_path) && !getOption("nfl4th.keep_games", !is_online)){
-    # remove games from package cache on load so it updates once
-    # does only run if options(nfl4th.keep_games) != TRUE and runner has access
-    # to github.com. If option is not set, the file will be removed if online.
+  } else if (file.exists(nfl4th_games_path) && !getOption("nfl4th.keep_games", FALSE)){
+    # remove games from package cache on load so it updates
+    # only runs if options(nfl4th.keep_games) != TRUE
+    # If option is not set, the file will be removed.
     file.remove(nfl4th_games_path)
   }
 }
