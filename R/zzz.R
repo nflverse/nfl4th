948be60462eb5b14dc06bcf49243eb489cb156cc
Tan Ho <38083823+tanho63@users.noreply.github.com>
2023-02-13 11:42:50 -0500

    Convert hardcoded paths to functions

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,3 @@
-nfl4th_cache_dir <- rappdirs::user_cache_dir("nfl4th", "nflverse")
-nfl4th_games_path <- file.path(nfl4th_cache_dir, "games_nfl4th.rds")
-nfl4th_fdmodel_path <- file.path(nfl4th_cache_dir, "fd_model.rds")
-nfl4th_wpmodel_path <- file.path(nfl4th_cache_dir, "wp_model.rds")
-
 .onLoad <- function(libname,pkgname){
   
   is_online <- !is.null(curl::nslookup("github.com", error = FALSE))
@@ -13,12 +8,12 @@ nfl4th_wpmodel_path <- file.path(nfl4th_cache_dir, "wp_model.rds")
   if(!is_online && keep_games) rlang::warn("GitHub.com seems offline, and `options(nfl4th.keep_games)` is set to TRUE. To get updates, clear the games cache with `nfl4th::nfl4th_clear_cache()`")
 
   # create package cache directory if it doesn't exist
-  if (!dir.exists(nfl4th_cache_dir)){
-    dir.create(nfl4th_cache_dir, recursive = TRUE, showWarnings = FALSE)
-  } else if (file.exists(nfl4th_games_path) && !keep_games){
+  if (!dir.exists(rappdirs::user_cache_dir("nfl4th", "nflverse"))){
+    dir.create(rappdirs::user_cache_dir("nfl4th", "nflverse"), recursive = TRUE, showWarnings = FALSE)
+  } else if (file.exists(nfl4th_games_path()) && !keep_games){
     # remove games from package cache on load so it updates
     # only runs if options(nfl4th.keep_games) != TRUE
     # If option is not set, the file will be removed.
-    file.remove(nfl4th_games_path)
+    file.remove(nfl4th_games_path())
   }
 }
