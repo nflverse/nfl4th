9dc64af269190d19ec00f8aa4a2126f3072c4de7
Tan Ho <38083823+tanho63@users.noreply.github.com>
2023-08-08 09:37:10 -0400

    Update and rename helper.R to setup.R

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,3 +1,10 @@
+## Limit datatable to two CPU cores for test purposes
+# https://github.com/Rdatatable/data.table/issues/5658
+
+current_threads <- data.table::getDTthreads()
+data.table::setDTthreads(2)
+testthat::teardown_env(data.table::setDTthreads(current_threads))
+
 play <-
   tibble::tibble(
     # things to help find the right game (use "reg" or "post")
